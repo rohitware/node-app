849d67d5f43e8dcd16b4f8c8df478df3c6e629f7
Rohit Ware <rohitware99@gmail.com>
2025-03-24 19:10:39 +0530

    Using fs.writeFile create output.txt file

--- fs-module.js
+++ fs-module.js
@@ -1,8 +1,21 @@
-var fs = require('fs');
-fs.readFile('sample.txt', 'utf8', function (err, data) {
-    if (err) throw err;
-    console.log(data);
+// var fs = require('fs');
+// fs.readFile('sample.txt', 'utf8', function (err, data) {
+//     if (err) throw err;
+//     console.log(data);
+// });
+
+
+
+// Import the filesystem module
+const fs = require("fs");
+
+let data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+
+fs.writeFile("output.txt", data, (err) => {
+    if (err) console.log(err);
+    else {
+        console.log("File written successfully\n");
+        console.log("The written file has the following contents:");
+        console.log(fs.readFileSync("output.txt", "utf8"));
+    }
 });
-
-
-
