1a64d863e89f94655b7bc9c654aeed39e75ac728
Rohit Ware <rohitware99@gmail.com>
2025-03-24 19:41:19 +0530

    The fs.appendFile() method

--- fs-module.js
+++ fs-module.js
@@ -1,10 +1,8 @@
- var fs = require('fs');
+var fs = require("fs");
 // fs.readFile('sample.txt', 'utf8', function (err, data) {
 //     if (err) throw err;
 //     console.log(data);
 // });
-
-
 
 // Import the filesystem module
 // const fs = require("fs");
@@ -20,18 +18,28 @@
 //     }
 // });
 
+// const filePath = 'output.txt';
 
-const filePath = 'output.txt';
+// fs.stat(filePath, (err, stats) => {
+//     if (err) {
+//         console.error('Error:', err);
+//         return;
+//     }
+//     console.log(`File size: ${stats.size} bytes`);
+//     console.log('Is a file:', stats.isFile());
+//     console.log('Is a directory:', stats.isDirectory());
+//     console.log(`File was created on: ${stats.birthtime}`);
+//     console.log(`File was last modified on: ${stats.mtime}`);
+//     console.log('File Information:', stats);
+// });
 
-fs.stat(filePath, (err, stats) => {
-    if (err) {
-        console.error('Error:', err);
-        return;
-    }
-    console.log(`File size: ${stats.size} bytes`);
-    console.log('Is a file:', stats.isFile());
-    console.log('Is a directory:', stats.isDirectory());
-    console.log(`File was created on: ${stats.birthtime}`);
-    console.log(`File was last modified on: ${stats.mtime}`);
-    console.log('File Information:', stats);
+const filePath = "output.txt";
+const newContent = " This data will append to file with dummy data\n";
+
+fs.appendFile(filePath, newContent, (err) => {
+  if (err) {
+    console.error("Error appending to file:", err);
+  } else {
+    console.log("data has been appended to the file successfully!");
+  }
 });
