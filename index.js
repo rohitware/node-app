37b34406f45ea81a851ff803a0f3e83677b02ac8
Rohit Ware <rohitware99@gmail.com>
2025-03-24 19:06:12 +0530

    fs read file

--- index.js
+++ index.js
@@ -4,7 +4,10 @@
 // console.log("====== os-module.js =======");
 // require('./os-module');
 
-console.log("====== path-module.js =======");
-require('./path-module');
+// console.log("====== path-module.js =======");
+// require('./path-module');
 
-console.log("My first node-app");
+console.log("====== fs-module.js =======");
+require('./fs-module');
+
+// console.log("My first node-app");
