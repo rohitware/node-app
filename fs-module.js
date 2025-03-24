af6406276e0f86bce3f2323ccd1c866d08b0c621
Rohit Ware <rohitware99@gmail.com>
2025-03-24 19:30:14 +0530

    fs stat() method

--- fs-module.js
+++ fs-module.js
@@ -1,4 +1,4 @@
-// var fs = require('fs');
+ var fs = require('fs');
 // fs.readFile('sample.txt', 'utf8', function (err, data) {
 //     if (err) throw err;
 //     console.log(data);
@@ -7,15 +7,31 @@
 
 
 // Import the filesystem module
-const fs = require("fs");
+// const fs = require("fs");
 
-let data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
+// let data = "Lorem ipsum dolor sit amet, consectetur adipiscing elit.";
 
-fs.writeFile("output.txt", data, (err) => {
-    if (err) console.log(err);
-    else {
-        console.log("File written successfully\n");
-        console.log("The written file contents:");
-        console.log(fs.readFileSync("output.txt", "utf8"));
+// fs.writeFile("output.txt", data, (err) => {
+//     if (err) console.log(err);
+//     else {
+//         console.log("File written successfully\n");
+//         console.log("The written file contents:");
+//         console.log(fs.readFileSync("output.txt", "utf8"));
+//     }
+// });
+
+
+const filePath = 'output.txt';
+
+fs.stat(filePath, (err, stats) => {
+    if (err) {
+        console.error('Error:', err);
+        return;
     }
+    console.log(`File size: ${stats.size} bytes`);
+    console.log('Is a file:', stats.isFile());
+    console.log('Is a directory:', stats.isDirectory());
+    console.log(`File was created on: ${stats.birthtime}`);
+    console.log(`File was last modified on: ${stats.mtime}`);
+    console.log('File Information:', stats);
 });
