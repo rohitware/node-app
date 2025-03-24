8745e4e779cbbb80bfa47d54fb10f194e75ffdd0
Rohit Ware <rohitware99@gmail.com>
2025-03-24 17:54:48 +0530

    Index file

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('first node app');
+// console.log("====== global-objects.js =======");
+// require('./global-objects');
+
+// console.log("====== os-module.js =======");
+// require('./os-module');
+
+console.log("====== path-module.js =======");
+require('./path-module');
+
+console.log("My first node-app");
